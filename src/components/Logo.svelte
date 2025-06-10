46f75112b4c571acdb66330e618fe8bf0582f4a8
Dmytro Sheleh <sheleg.dmitriy@gmail.com>
2025-06-10 16:29:15 +0200

    feat: add new logo

---
+++ src/components/Logo.svelte
@@ -0,0 +1,4 @@
+<script lang="ts">
+	import logo from '../assets/logo.svg';
+</script>
+<img src="{logo}" alt="ASIMOV Systems Logo" class="w-36" />
--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,19 +1,21 @@
+<script lang="ts">
+	import Logo from './Logo.svelte';
+	const CONTACT_US_EMAIL = import.meta.env.VITE_CONTACT_US_EMAIL || '';
+</script>
 <nav class="border-oOrange-500 border-b-2 shadow-sm">
 	<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
 		<div class="flex h-16 items-center justify-between">
 			<div class="flex-shrink-0">
-				<div class="font-arges text-2xl font-bold tracking-wider text-white uppercase">
-					<span>ASIMOV.</span>
-					<span>Systems</span>
-				</div>
+				<Logo />
 			</div>
 
 			<div class="flex items-center">
-				<button
+				<a
+					href="mailto:{CONTACT_US_EMAIL}"
 					class="bg-oOrange-500 hover:bg-sSlate-800 font-arges cursor-pointer rounded-lg px-6 py-2 text-xl font-bold tracking-wider text-white uppercase transition-colors duration-200"
 				>
 					Get in touch
-				</button>
+				</a>
 			</div>
 		</div>
 	</div>
