83800710f266491f458a9628c9bc6f07e5e4d4f2
Dmytro <34593263+shelegdmitriy@users.noreply.github.com>
2025-06-11 19:17:53 +0300

    chore: copy CNAME during deploy into public folder (#8)

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import tailwindcss from '@tailwindcss/vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [svelte(), tailwindcss()]
+	plugins: [
+		svelte(),
+		tailwindcss(),
+		viteStaticCopy({
+			targets: [
+				{
+					src: path.resolve(__dirname, 'CNAME'),
+					dest: '.'
+				}
+			]
+		})
+	]
 });
